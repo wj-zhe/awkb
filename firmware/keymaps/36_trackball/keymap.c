70fa4b4fc96af7bb5975e887ff955bc2b96ae507
wj-zhe <saumoon@gmail.com>
2025-10-17 20:57:01 +0800

    Removed mouse button keys on several layers

--- firmware/keymaps/36_trackball/keymap.c
+++ firmware/keymaps/36_trackball/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             LGUI_T(KC_Q), KC_W, KC_E, KC_R, KC_T,          KC_Y, KC_U, KC_I, KC_O, LGUI_T(KC_P),
             LCTL_T(KC_A), KC_S, KC_D, KC_F, KC_G,          KC_H, KC_J, KC_K, KC_L, LCTL_T(KC_SCLN),
             LSFT_T(KC_Z), KC_X, KC_C, KC_V, LT(3,KC_B),    KC_N, KC_M, KC_COMM, KC_DOT, LSFT_T(KC_SLSH),
-                 LT(2,KC_TAB), LALT_T(KC_BSPC), ENC_TG,    KC_BTN1, LALT_T(KC_SPC), TD(TD_L1)
+                 LT(2,KC_TAB), LALT_T(KC_BSPC), ENC_TG,    KC_NO, LALT_T(KC_SPC), TD(TD_L1)
 
             ),
 
@@ -45,14 +45,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
             LCTL_T(KC_1), KC_2, KC_3, KC_4, KC_5,           KC_6, KC_7, KC_8, KC_9, LCTL_T(KC_0),
             KC_LSFT, KC_GRV, KC_BSLS, KC_LBRC, KC_RBRC,     KC_EQL, KC_MINS, ___, ___, ___,
-                                      KC_LGUI, ___, ___,    KC_BTN2, ___, TG(1)
+                                      KC_LGUI, ___, ___,    KC_NO, ___, TG(1)
             ),
 
     [2] = LAYOUT_36(
             LGUI_T(KC_F9), KC_F10, KC_F11, KC_F12, KC_PSCR,    KC_HOME, KC_PGDN, KC_PGUP, KC_END, LGUI_T(KC_WBAK),
             LCTL_T(KC_F5), KC_F6, KC_F7, KC_F8, KC_INS,        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, LCTL_T(KC_WFWD),
             LSFT_T(KC_F1), KC_F2, KC_F3, KC_F4, TO(3),         TD(TD_MD), KC_VOLD, KC_VOLU, KC_MUTE, KC_LSFT,
-                                        TG(2), KC_DEL, ___,    KC_BTN2, LALT_T(KC_APP), KC_DEL
+                                        TG(2), KC_DEL, ___,    KC_NO, LALT_T(KC_APP), KC_DEL
 
             ),
 
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             ),
 
     [4] = LAYOUT_36(
-            KC_LGUI, KC_NO, KC_NO, KC_NO, MS_BTN3,      KC_NO, KC_NO, KC_NO, KC_NO, KC_RGUI,
+            KC_LGUI, KC_NO, KC_NO, KC_NO, MS_BTN3,      MS_BTN1, MS_BTN2, KC_NO, KC_NO, KC_RGUI,
             KC_LCTL, KC_NO, KC_NO, MS_BTN1, MS_BTN2,    KC_NO, KC_NO, KC_NO, KC_NO, KC_RCTL,
             KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_RSFT,
                           MS_BTN1, MS_BTN2, KC_NO,      KC_NO, KC_NO, KC_NO
